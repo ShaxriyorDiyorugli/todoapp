c71343ee5596f4e4b15176a0c4b4ca01b308024d
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-07-09 01:43:24 +0500

    Models qismi yozildi

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TODO(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"ToDo of {self.user.username}"
