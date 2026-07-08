9211dbc929a2a742b4260a7d3bca5b645d0c675f
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-07-09 01:50:33 +0500

    admin panel yozildi

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TODO
 
-# Register your models here.
+@admin.register(TODO)
+class TodoAdmin(admin.ModelAdmin):
+    model = TODO
+    list_display = ('date', 'body', 'done')
+
+
